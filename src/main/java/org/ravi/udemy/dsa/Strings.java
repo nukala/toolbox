c607e3bc1b78f2d3352a14f334a9412f5aeeffe1
ravi <nukala@gmail.com>
2019-08-31 17:09:34 -0700

    Refactored to new packages hasOnlyDigits method arrived

--- src/main/java/org/ravi/udemy/dsa/Strings.java
+++ src/main/java/org/ravi/udemy/dsa/Strings.java
@@ -1,10 +1,11 @@
 package org.ravi.udemy.dsa;
 
 import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.ravi.udemy.dsa.WorthLooking;
+import java.util.regex.Pattern;
 
 public class Strings {
     // https://www.udemy.com/master-the-coding-interview-data-structures-algorithms/learn/lecture/12308750#content
@@ -21,7 +22,7 @@ public class Strings {
         return sb.toString();
     }
 
-    // https://javarevisited.blogspot.com/2015/08/how-to-find-all-permutations-of-string-java-example.html
+    // javarevisited.blogspot.com/2015/08/how-to-find-all-permutations-of-string-java-example.html
     @WorthLooking("all permutations of a string using recursive helper")
     public static List<String> permutations(String input) {
         return permutations("", input);
@@ -46,12 +47,12 @@ public class Strings {
 
     // From "ABCD": A, AB, ABC, ABCD, B, BC, BCD, C, CD, D
     // actually asked in a video-interview
-     @WorthLooking("substrings from a big str")
+    @WorthLooking("substrings from a big str")
     public static List<String> substrings(String input) {
         if ((input == null) || (input.length() == 0)) {
             return Lists.newArrayList(input);
         }
-        List<String> list = new ArrayList();
+        List<String> list = new ArrayList<>();
 
         int len = input.length();
         for (int i = 0; i < len; i++) {
@@ -64,5 +65,17 @@ public class Strings {
             }
         }
         return list;
+    }
+
+    // javarevisited.blogspot.com/2012/10/regular-expression-example-in-java-to-check-String-number.html
+    // regex listed there does not work with java8-162
+    public static boolean hasOnlyDigits(String str) {
+        if (StringUtils.isEmpty(str)) {
+            return false;
+        }
+
+        String pattern = "^[0-9]*";
+        return Pattern.compile(pattern)
+                .matcher(str).matches();
     }
 }
