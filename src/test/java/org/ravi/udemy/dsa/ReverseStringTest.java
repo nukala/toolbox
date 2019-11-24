3637fdab8964eca12a2a504fc695c546c8ee9c68
ravi <nukala@gmail.com>
2019-11-24 14:51:18 -0800

    Efficient string reverse arrived old one got renamed test added

--- src/test/java/org/ravi/udemy/dsa/ReverseStringTest.java
+++ src/test/java/org/ravi/udemy/dsa/ReverseStringTest.java
@@ -3,33 +3,46 @@ package org.ravi.udemy.dsa;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.ravi.udemy.dsa.Strings.reverseSlow;
 import static org.ravi.udemy.dsa.Strings.reverse;
 
-
 public class ReverseStringTest {
-
-
     @Test
     public void reverseThis() {
-        assertThat(reverse("this"))
+        assertThat(reverseSlow("this"))
                 .isEqualTo("siht");
     }
 
     @Test
     public void reverseHello() {
-        assertThat(reverse("hello"))
+        assertThat(reverseSlow("hello"))
                 .isEqualTo("olleh");
     }
 
     @Test
-    public void reverseNull() {
-        assertThat(reverse(null))
+    public void reverseEmpty() {
+        assertThat(reverseSlow(null))
                 .isNull();
+        assertThat(reverseSlow(""))
+                .isEmpty();
     }
 
     @Test
     public void reverseFromTheCourse() {
-        assertThat(reverse("Hi My name is Andrei"))
+        assertThat(reverseSlow("Hi My name is Andrei"))
                 .isEqualTo("ierdnA si eman yM iH");
+    }
+
+    @Test
+    public void reverseEfficientTests() {
+        assertThat(reverse(null))
+                .isNull();
+        assertThat(reverse(""))
+                .isEmpty();
+
+        assertThat(reverse("Hello"))
+                .isEqualTo("olleH");
+        assertThat(reverse("ravi"))
+                .isEqualTo("ivar");
     }
 }
