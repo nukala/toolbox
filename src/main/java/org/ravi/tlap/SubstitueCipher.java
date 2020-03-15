ecabfd16a932e9500787c5a3003fc797955b0ad6
ravi <nukala@gmail.com>
2020-03-15 14:22:56 -0700

    new code - LineStats, IntList and nop-substitue cipher

---
+++ src/main/java/org/ravi/tlap/SubstitueCipher.java
@@ -0,0 +1,10 @@
+package org.ravi.tlap;
+
+/**
+ * exercise 3.4: Here’s a variation on the array of const values. Write a program for creating a substitution cipher problem. In a substitution cipher problem, all messages are made of uppercase letters and punctuation. The original message is called the plaintext, and you create the ciphertext by substituting each letter with another letter (for example, each C could become an X). For this problem, hard-code a const array of 26 char elements for the cipher, and have your program read a plaintext message and output the equivalent ciphertext.
+ * <br/>
+ * p78, pdf=98
+ */
+public class SubstitueCipher {
+    //RNTODO
+}
