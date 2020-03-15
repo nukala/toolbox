940dbe70b0b3a431240f2f68c631d99fd422faf2
ravi <nukala@gmail.com>
2020-03-15 14:19:20 -0700

    static converted into class methods

--- src/test/java/org/ravi/tlap/LuhnValidationTest.java
+++ src/test/java/org/ravi/tlap/LuhnValidationTest.java
@@ -1,41 +1,57 @@
 package org.ravi.tlap;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.ravi.tlap.LuhnValidation.nearest10;
-import static org.ravi.tlap.LuhnValidation.twiceSum;
 
 public class LuhnValidationTest {
+    LuhnValidation luhn = new LuhnValidation();
+
     @Test
     public void nearestTests() {
-        assertThat(nearest10(27)).isEqualTo(3);
+        assertThat(luhn.nearest10(27)).isEqualTo(3);
 
-        assertThat(nearest10(1)).isEqualTo(9);
-        assertThat(nearest10(0)).isEqualTo(0);
+        assertThat(luhn.nearest10(1)).isEqualTo(9);
+        assertThat(luhn.nearest10(0)).isEqualTo(0);
     }
 
     @Test
     public void twiceSumTest() {
-        assertThat(twiceSum("1234", 1)).isEqualTo(8);
+        assertThat(luhn.twiceSum("1234", 1)).isEqualTo(8);
 
-        assertThat(twiceSum("786", 1)).isEqualTo(3);
-        assertThat(twiceSum("786", 2)).isEqualTo(7);
-        assertThat(twiceSum("786", 3)).isEqualTo(5);
+        assertThat(luhn.twiceSum("786", 1)).isEqualTo(3);
+        assertThat(luhn.twiceSum("786", 2)).isEqualTo(7);
+        assertThat(luhn.twiceSum("786", 3)).isEqualTo(5);
 
-        assertThat(twiceSum("786", 4)).isEqualTo(-1);
-        assertThat(twiceSum("786", 0)).isEqualTo(-1);
-        assertThat(twiceSum("786", -3)).isEqualTo(-1);
+        assertThat(luhn.twiceSum("786", 4)).isEqualTo(-1);
+        assertThat(luhn.twiceSum("786", 0)).isEqualTo(-1);
+        assertThat(luhn.twiceSum("786", -3)).isEqualTo(-1);
     }
 
     @Test
-    public void roundTrip() {
-        LuhnValidation validation = new LuhnValidation();
+    public void bookSample() {
         int id = 176248;
-        assertThat(validation.getSum(id)).isEqualTo(3);
+        assertThat(luhn.getSum(id)).isEqualTo(3);
 
         int withSum = id * 10 + 3;
-        assertThat(validation.validate(withSum)).isTrue();
+        assertThat(luhn.validate(withSum)).isTrue();
     }
 
+    @Test
+    public void wikipediaSample() {
+        String idStr = "7992739871";
+
+        assertThat(luhn.getSum(idStr)).isEqualTo(3);
+        assertThat(luhn.validate(idStr + "3")).isTrue();
+    }
+
+    @Ignore("Full of meaningless values?")
+    public void simcardFromWikipedia() {
+        //assertThat(luhn.getSum("896101950123440000")).isEqualTo(1);
+        assertThat(luhn.getSum("950123440000")).isEqualTo(8);
+
+        assertThat(luhn.validate("9501234400008")).isTrue();
+        assertThat(luhn.validate("8961019501234400001")).isTrue();
+    }
 }
